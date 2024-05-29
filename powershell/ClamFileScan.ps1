306a2cd206f900fc2eb3529c75e2be86b1205664
Michael Astfalk <mtastfalkjr@verizon.net>
2024-05-29 14:27:15 -0400

    Create ClamFileScan.ps1
    Began working on a powershell script to work with Clam AV.

---
+++ powershell/ClamFileScan.ps1
@@ -0,0 +1,8 @@
+# Clam File Scan
+# Initiates a clam scan on the entered file. The script must be run in the directory where the ClamAV
+# files are located or you must change the path to the exe files.
+# Created: 5/29/24
+# Updated: 5/29/24
+# Created by calicojack1720
+
+Start-Process -NoNewWindow .\clamd.exe
