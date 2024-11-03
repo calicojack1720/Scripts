529c387db8e9f59c9484fe9908cde024011bea10
Michael Astfalk <mtastfalkjr@verizon.net>
2024-11-03 09:56:11 -0500

    Update GetPractice.py
    Added values and reformatted columns

--- python/GetPractice.py
+++ python/GetPractice.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 # URL of the page to scrape
 url = input("Enter the url: ")
 race_name = input("Enter the name of the race: ")
+session = input("Enter the session: ")
 
 # Send a GET request to the URL
 response = requests.get(url)
@@ -20,7 +21,7 @@ if response.status_code == 200:
     # Find and print all the practice session results
     results_table = soup.find(id="maincontent")
     if results_table:
-        print(f"Position,Car No, Time, Gap, Laps")
+        print(f"Race,Position,Car No, Time, Gap, Laps,Session")
         rows = results_table.find_all('tr')
         for row in rows:
             columns = row.find_all('td')
@@ -31,7 +32,7 @@ if response.status_code == 200:
                 gap = columns[5].text.strip()
                 laps = columns[6].text.strip()
                 # print(f"Position: {position}, Car No: {car_no}, Time: {time}, Gap: {gap}, Laps: {laps}")
-                print(f"{race_name},{position},{car_no},{time},{gap},{laps}")
+                print(f"{race_name},{position},{car_no},{time},{gap},{laps},{session}")
                 
     else:
         print("No results found!")
